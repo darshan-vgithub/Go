e0c25b87d7fd49d6f00e549630e40ed6ae97d6d9
Darshan.V <darshanv487@gmail.com>
2024-09-20 11:21:31 +0530

    started practicing go

---
+++ hello/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	//string
+	var agency string="Fast Track"
+	fmt.Println("Welcome to",agency)
+	
+	// practice
+	var name="Darshan"
+	fmt.Println(name)
+	
+	name1:="Darshan" // variable name and : shorthand property to declare a variable 
+	fmt.Println(name1)
+	fmt.Println("Hello,",name1) 
+
+	totalCars:=50
+	fmt.Println("Our fleet consists of ",totalCars)
+
+	StartingPrice:=29.99
+
+	fmt.Printf("Our cars starting price is %v \n",StartingPrice)
+	fmt.Println("take your pick")
+
+	name2:="zoro"
+	fmt.Printf("where are you from %v\n",name2)
+	fmt.Println("welcome")
+}
