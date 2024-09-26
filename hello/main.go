f3fcd80f2afb6509980a823128fb8621267f745f
Darshan.V <darshanv487@gmail.com>
2024-09-26 11:13:37 +0530

    started the go lang freshly

--- hello/main.go
+++ hello/main.go
@@ -3,43 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	//string
-	var agency string="Fast Track"
-	fmt.Println("Welcome to",agency)
-	
-	// practice
-	var name="Darshan"
-	fmt.Println(name)
-	
-	name1:="Darshan" // variable name and : shorthand property to declare a variable 
-	fmt.Println(name1)
-	fmt.Println("Hello,",name1) 
-
-	totalCars:=50
-	fmt.Println("Our fleet consists of ",totalCars)
-
-	StartingPrice:=29.99
-
-	fmt.Printf("Our cars starting price is %v \n",StartingPrice)
-	fmt.Println("take your pick")
-
-	includesInsurance:=false
-	fmt.Println("the insurance is",includesInsurance)
-
-
-	var name2 string
-	fmt.Println("name2",name2)
-
-	var number int
-	fmt.Println("number",number)
-
-	var price float64
-	fmt.Println("price",price)
-
-	var isActive bool
-	fmt.Println("isActive",isActive)
-
-	// more on strings
-
-	moreOnStrings()
+	fmt.Println("Hello, I am learning Go programming language")
 }
