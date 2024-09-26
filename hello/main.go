274f08bc407763e823ee9f322a45519f0ff6cc4f
Darshan.V <darshanv487@gmail.com>
2024-09-26 12:54:03 +0530

    got to know what all data types are there and what all the advance data types re therw in the go lang

--- hello/main.go
+++ hello/main.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, I am learning Go programming language")
+
+	// datayptes=
+	// string
+	// bool
+	// integer (unint8, unint16, unint32, unint64, int8, int16, int32, int64, unintptr)
+	// alias also involved in integer
+	// float (float32, float64)
+	// complex
+
+	// Advance datatypes like=
+	// array
+	// slice
+	// map
+	// struct
+	// pointer
+	// interface
+	// channel
+
 }
