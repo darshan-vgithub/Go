2465dd9e9edd0c19aa749624d5b674866e9317c0
Darshan.V <darshanv487@gmail.com>
2024-09-26 17:06:28 +0530

    made the user input using scan

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -18,5 +18,13 @@ func main() {
 //comma ok syntax or comma error syntax
 	input,_:=raeder.ReadString('\n')  // input is like a try and _ is like a catch instead of _ we can slo write err or we can also write like _,_
 	fmt.Println("Thanks for rating, ",input)
-	fmt.Printf("the type of this variable is : %T",input)
+	fmt.Printf("the type of this variable is : %T",input,"\n")
+
+
+
+	 scanner := bufio.NewScanner(os.Stdin)
+    fmt.Print("Enter your name: ")
+    scanner.Scan()
+    name := scanner.Text()
+    fmt.Println("Hello, ", name)
 }
