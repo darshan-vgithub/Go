c10c03d530eaf8d258fb1c98e00372f279ca5232
Darshan.V <darshanv487@gmail.com>
2024-09-27 11:18:39 +0530

    05mytime/main.go:using scanner and strconv package made print the using user input

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+)
 
 func main() {
-	fmt.Println("Hello welcome to my time")
+    raeder := bufio.NewScanner(os.Stdin)
+
+    fmt.Println("Enter your name:")
+    raeder.Scan()
+    name := raeder.Text()
+    fmt.Println("Your name is", name)
+
+    fmt.Println("Enter your age:")
+    raeder.Scan()
+    agestr := raeder.Text()
+
+    age, err := strconv.Atoi(agestr)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println("Your age is", age) // Print the age variable instead of agestr
+
+    fmt.Println("Enter your designation:")
+    raeder.Scan()
+    designation := raeder.Text()
+    fmt.Println("Your designation is", designation)
+
+
+	fmt.Printf("my name is %v and my age is %v and my designation is %v", name, age, designation)
 }
