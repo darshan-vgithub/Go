5f2b01817f13306237c59d297fe917e2a6d7d756
Darshan.V <darshanv487@gmail.com>
2024-09-27 12:14:50 +0530

    now will start understanding time package

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -1,37 +1,39 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-    raeder := bufio.NewScanner(os.Stdin)
+	raeder := bufio.NewScanner(os.Stdin)
 
-    fmt.Println("Enter your name:")
-    raeder.Scan()
-    name := raeder.Text()
-    fmt.Println("Your name is", name)
+	fmt.Println("Enter your name:")
+	raeder.Scan()
+	name := raeder.Text()
+	fmt.Println("Your name is", name)
+	fmt.Printf("the type of this variable is %T", name)
 
-    fmt.Println("Enter your age:")
-    raeder.Scan()
-    agestr := raeder.Text()
+	fmt.Println("Enter your age:")
+	raeder.Scan()
+	agestr := raeder.Text()
 
-    age, err := strconv.Atoi(agestr)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	age, err := strconv.Atoi(agestr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    fmt.Println("Your age is", age) // Print the age variable instead of agestr
+	fmt.Println("Your age is", age) // Print the age variable instead of agestr
+	fmt.Printf("the type of this variable is %T", age,"\n")
 
-    fmt.Println("Enter your designation:")
-    raeder.Scan()
-    designation := raeder.Text()
-    fmt.Println("Your designation is", designation)
-
+	fmt.Println("Enter your designation:")
+	raeder.Scan()
+	designation := raeder.Text()
+	fmt.Println("Your designation is", designation)
+	fmt.Printf("the type of this variable is %T", designation)
 
 	fmt.Printf("my name is %v and my age is %v and my designation is %v", name, age, designation)
 }
