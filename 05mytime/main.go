16ce9f915ca5a3dce2f33eed367b50074e1759ef
Darshan.V <darshanv487@gmail.com>
2024-09-30 13:37:50 +0530

    minor formating changes

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("present time is : ", PresentTime)
 
 	fmt.Println(PresentTime.Format("Mon Jan 2 15:04:05 MST 2006"))
-loc := time.Location{}
-CreatedTime := time.Date(2022, time.December, 22, 0, 0, 0, 0, &loc)
+	loc := time.Location{}
+	CreatedTime := time.Date(2022, time.December, 22, 0, 0, 0, 0, &loc)
 	fmt.Println("created time is : ", CreatedTime)
 }
